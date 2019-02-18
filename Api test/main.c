41d5301fd30f46d2df01530c41b50176a0979271
Jonas <jonasnizard@gmail.com>
2019-02-18 14:39:15 +0100

    faf
    dzfza

--- Api test/main.c
+++ Api test/main.c
@@ -1,72 +1,68 @@
 #include <stdio.h>
-#include <curl/curl.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "cJSON.h"
+#include <curl/curl.h>
+
+
 void zerofgets(char * str, int size)
 {
     fflush(stdin);
     fgets(str, size, stdin);
     str[strcspn(str, "\n")] = 0;
 }
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#include <curl/curl.h>
-
 static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
 {
-  size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
-  return written;
+    size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
+    return written;
 }
 
 int main(int argc, char *argv[])
 {
-  CURL *curl_handle;
-  static const char *pagefilename = "page.txt";
-  FILE *pagefile;
+    CURL *curl_handle;
+    static const char *pagefilename = "page.JSON";
+    FILE *pagefile;
 
-  if(argc < 2) {
-    printf("Usage: %s <URL>\n", argv[0]);
-    return 1;
-  }
 
-  curl_global_init(CURL_GLOBAL_ALL);
 
-  /* init the curl session */
-  curl_handle = curl_easy_init();
+    curl_global_init(CURL_GLOBAL_ALL);
 
-  /* set URL to get here */
-  curl_easy_setopt(curl_handle, CURLOPT_URL, "https://restcountries.eu/rest/v2/name/france");
+    /* init the curl session */
+    curl_handle = curl_easy_init();
 
-  /* Switch on full protocol/debug output while testing */
-  curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
+    /* set URL to get here */
+    curl_easy_setopt(curl_handle, CURLOPT_URL, "http://restcountries.eu/rest/v2/name/france");
 
-  /* disable progress meter, set to 0L to enable and disable debug output */
-  curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
+    /* Switch on full protocol/debug output while testing */
+    curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
 
-  /* send all data to this function  */
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
+    /* disable progress meter, set to 0L to enable and disable debug output */
+    curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
 
-  /* open the file */
-  pagefile = fopen(pagefilename, "w+");
-  if(pagefile) {
+    /* send all data to this function  */
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
 
-    /* write the page body to this file handle */
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, pagefile);
+    /* open the file */
+    pagefile = fopen(pagefilename, "wb");
+    if(pagefile)
+    {
 
-    /* get it! */
-    curl_easy_perform(curl_handle);
+        /* write the page body to this file handle */
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, pagefile);
 
-    /* close the header file */
-    fclose(pagefile);
-  }
+        /* get it! */
+        curl_easy_perform(curl_handle);
 
-  /* cleanup curl stuff */
-  curl_easy_cleanup(curl_handle);
+        /* close the header file */
+        fclose(pagefile);
+    }
 
-  curl_global_cleanup();
+    /* cleanup curl stuff */
+    curl_easy_cleanup(curl_handle);
 
-  return 0;
+    curl_global_cleanup();
+
+    return 0;
 }
 
 
